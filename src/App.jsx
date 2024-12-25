c93429e9d16c1bcfe41df11744b06b8bdc32805f
Bar Yosef Tayar Mac <bartayar123456789@gmail.com>
2024-12-25 15:03:44 +0200

    Birthday-reminder

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import data from './data';
+import { useState } from 'react';
+import List from './List';
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+  
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}/>
+        <button className="btn btn-block" type="button" onClick={()=>setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
+};
+export default App;
